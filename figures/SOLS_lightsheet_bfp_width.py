904fc7d70d1775784cc3a8f5d614427c6e72960b
Alfred Millett-Sikking <amsikking@gmail.com>
2023-10-24 10:10:53 -0700

    Update SOLS_lightsheet_bfp_width.py
    Bug fix: 'f' replaced with 'f_mm' everywhere (the function arg).

--- figures/SOLS_lightsheet_bfp_width.py
+++ figures/SOLS_lightsheet_bfp_width.py
@@ -2,7 +2,7 @@ import numpy as np
 from SOLS_optimum_tilt import optimum_tilt
 
 def lightsheet_bfp_width(f_mm, n, theta_x, theta_t, verbose=True):
-    x_mm = 2 * f * n * np.sin(theta_t) * theta_x
+    x_mm = 2 * f_mm * n * np.sin(theta_t) * theta_x
     if verbose:
         print('Light-sheet BFP width ' +
               '(f_mm=%0.2f, theta_t=%0.2f, theta_x=%0.2f):'%(
@@ -13,16 +13,16 @@ def lightsheet_bfp_width(f_mm, n, theta_x, theta_t, verbose=True):
 
 if __name__ == "__main__":
     # Nikon 100x1.35 Silicone:
-    na, n, f = 1.35, 1.41, 2
+    na, n, f_mm = 1.35, 1.41, 2
     theta_1, theta_e, theta_x, theta_t = optimum_tilt(na, n)
-    lightsheet_bfp_width(f, n, theta_x, theta_t)
+    lightsheet_bfp_width(f_mm, n, theta_x, theta_t)
 
     # Nikon 40x1.15 Water:
-    na, n, f = 1.15, 1.33, 5
+    na, n, f_mm = 1.15, 1.33, 5
     theta_1, theta_e, theta_x, theta_t = optimum_tilt(na, n)
-    lightsheet_bfp_width(f, n, theta_x, theta_t)
+    lightsheet_bfp_width(f_mm, n, theta_x, theta_t)
 
     # Nikon 25x1.00 Water:
-    na, n, f = 1.00, 1.33, 8
+    na, n, f_mm = 1.00, 1.33, 8
     theta_1, theta_e, theta_x, theta_t = optimum_tilt(na, n)
-    lightsheet_bfp_width(f, n, theta_x, theta_t)
+    lightsheet_bfp_width(f_mm, n, theta_x, theta_t)
